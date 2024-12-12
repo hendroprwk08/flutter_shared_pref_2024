90c98f66a918ab137bf7a2321cb5c873291318b8
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2024-12-12 08:46:27 +0700

    ok

---
+++ android/app/src/main/kotlin/com/example/flutter_shared_pref_2024/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_shared_pref_2024
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+         backgroundColor: Colors.cyan,
+        title: Text(widget.title, style: TextStyle(color: Colors.white),),
+      ),
+      body: Center(child: Text(widget.title),)
+    );
+  }
+}
